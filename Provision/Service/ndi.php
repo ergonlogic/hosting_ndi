00990328c8fa63fcc627e2b0d6997a03b5d70e81
Christopher Gervais <chris@ergonlogic.com>
2014-06-19 17:31:26 +0000

    Initial commit.

---
+++ Provision/Service/ndi.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * The ndi service class.
+ */
+class Provision_Service_ndi extends Provision_Service {
+  public $service = 'ndi';
+
+  /**
+   * Add the needed properties to the site context.
+   */
+  static function subscribe_site($context) {
+    $context->setProperty('ndi_civi_ext');
+  }
+}
